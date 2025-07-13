e19d24534094c557c0e4af81c1ab19c82ae94d1d
phpablo <pablohenrique120@gmail.com>
2025-07-13 19:48:47 -0300

    :pencil: resolve desafio

---
+++ projetos-pessoais/discord/desafios/001_soma_dois_valores.py
@@ -0,0 +1,21 @@
+# Autor : Pablo Araujo
+# Data : 13JUL25
+# DESAFIO 001 - Crie um algoritmo para somar dois números inteiros.
+# Seu código deve receber 2 números inteiros, somar e retornar o resultado
+# Utilize a linguagem que você quiser e peça ajuda se precisar.
+
+# Passos
+# somar dois numeros
+# receber numero 1
+# receber numero 2
+# somar os dois numeros 
+# mostrar na tela 
+
+n1 = 3
+n2 = 4
+
+res = n1 + n2
+
+print('=== SOMADOR DE DOIS NÚMEROS ===')
+print('O número',n1, ' e ', n2, ' somados são : ')
+print(res)
